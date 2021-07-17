a9fd27fc0e9310ceab49fa368237fef72be31b7b
ExequielGonzalez <gonzalez-exequiel@hotmail.com>
2021-07-17 01:24:19 -0300

    get method working properly

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const Constants = require("./constants.js");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
 
 let usuario = {
   nombre: "",
